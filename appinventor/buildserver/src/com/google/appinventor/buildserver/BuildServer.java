5f6f5df1cc2a39e2c6ff2205d28c49cd3085b454
Jim Miller <James.Miller@alum.mit.edu>
2014-10-12 17:10:37 -0700

    Handle case where server doesn't have public name
    Changed start-up code to use localhost instead of http://… when no name
    is public for the computer

--- appinventor/buildserver/src/com/google/appinventor/buildserver/BuildServer.java
+++ appinventor/buildserver/src/com/google/appinventor/buildserver/BuildServer.java
@@ -552,7 +552,11 @@ public class BuildServer {
 
     int port = commandLineOptions.port;
     SelectorThread threadSelector = GrizzlyServerFactory.create("http://localhost:" + port + "/");
-    String hostAddress = InetAddress.getLocalHost().getHostAddress();
+    String hostAddress = "localhost";
+    try {
+    hos	tAddress = "http://" + InetAddress.getLocalHost().getHostAddress();
+    } catch (Exception e) {
+    }
     LOG.info("App Inventor Build Server - Version: " + GitBuildId.getVersion());
     LOG.info("App Inventor Build Server - Git Fingerprint: " + GitBuildId.getFingerprint());
     LOG.info("Running at: http://" + hostAddress + ":" + port + "/buildserver");
@@ -561,9 +565,9 @@ public class BuildServer {
     } else {
       LOG.info("Maximum simultanous builds = " + commandLineOptions.maxSimultaneousBuilds);
     }
-    LOG.info("Visit: http://" + hostAddress + ":" + port +
+    LOG.info("Visit: " + hostAddress + ":" + port +
       "/buildserver/health for server health");
-    LOG.info("Visit: http://" + hostAddress + ":" + port +
+    LOG.info("Visit: " + hostAddress + ":" + port +
       "/buildserver/vars for server values");
     LOG.info("Server running");
   }
